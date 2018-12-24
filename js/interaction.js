64520a7d27823a0c7d3d951191c1af74c4d949d8
thvardhan <thvardhan.god@gmail.com>
2018-12-24 16:02:51 +0530

    attach model listeners to eggs page

--- js/interaction.js
+++ js/interaction.js
@@ -1,11 +1,19 @@
 var rootVue = new Vue({
   el : '#appRoot',
   data : {
-    eggName : ''
+    eggName : '',
+    eggStyle : '',
+    animate : true,
+    auto : false,
+    hatchBackup : false,
+    hatchBupLevel : 4,
+    goal : false,
+    goalLevel : 4,
+    reachLevelTimes : 50
   },
   methods : {
     hello : function(){
-      alert(this.eggName);
+      alert(this.hatchBupLevel);
     }
   },
 
